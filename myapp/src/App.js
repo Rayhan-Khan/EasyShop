eae02131c2b5677247fd196aa2d6c82b0072a44f
Rayhan Khan <rayhankhan014@gmail.com>
2022-12-27 20:03:21 +0600

    use axios to fetching data and pass data home component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,25 @@
+import axios from "axios";
 import Cookies from "js-cookie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DetailsProduct, Header, Home, Login, Product, Siginup } from "./Components";
+import { baseUrl } from "./utils/baseurl";
 import NotLoggedin from "./utils/Notloggedin";
 
 function App() {
+  const [data,setData]=useState([]);
   const [phone,setPhone]=useState(Cookies.get('phone'));
+  useEffect(()=>{
+    (async function(){
+     const res= await axios.get(`${baseUrl}/getallproduct`);
+        setData(res.data.data)
+    }())
+  },[]);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header phone={phone} setPhone={setPhone}/>}>
-             <Route index element={<Home />} />
+             <Route index element={<Home data={data}/>} />
              <Route path="/products" element={< Product/>} />
              <Route path="/products/:categories" element={< Product/>} />
              <Route path="/products/:id" element={< DetailsProduct/>} />
